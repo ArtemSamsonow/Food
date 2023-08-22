04e1b1dd30520eef7776c1c75587065422e6f8c4
Rafshik <artem.samsonow25@yandex.ru>
2023-08-22 17:17:34 +0300

    Complete Header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <HeadApp></HeadApp>>
+  <HeadApp></HeadApp>
 </template>
 
 <script>
@@ -12,6 +12,28 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+*{
+  margin: 0;
+  font-family: 'Roboto Slab', serif;
+}
+body{
+  width: 100%;
+}
+.app{
+  padding: 30px 50px 0 50px;
+  a{
+    text-decoration: none;
+    color: #CC693E;
+  }
+  h3{
+    font-size: 18px;
+  }
+  p{
+    font-size: 12px;
+  }
+  .context{
+    color: #666666;
+  }
+}
 </style>
--- src/components/HeadApp.vue
+++ src/components/HeadApp.vue
@@ -1,11 +1,53 @@
 <script setup>
-const add = "";
+import logoImg from "@/assets/Logo.svg";
+import locateImg from "@/assets/locate.svg";
 </script>
 
 <template>
-  <input v-model="add">
+  <div class="header">
+
+    <div class="header__logo">
+      <img :src="logoImg" alt="Логотип компании">
+    </div>
+
+    <div class="header__locate">
+      <img :src="locateImg" alt="">
+      <p class = "context"><a href="">Ховрино </a>Круглосуточно</p>
+      <h3>+7 (495) 980-66-22</h3>
+    </div>
+
+    <div class="header__locate">
+      <img :src="locateImg" alt="">
+      <p class = "context"><a href="">Измайлово </a>Круглосуточно</p>
+      <h3>+7 (495) 980-66-22</h3>
+    </div>
+
+    <div class="header__locate">
+      <img :src="locateImg" alt="">
+      <p class = "context"><a href="">Мытищи </a>Круглосуточно</p>
+      <h3>+7 (495) 980-66-22</h3>
+    </div>
+
+    <div class="header__locate">
+      <img :src="locateImg" alt="">
+      <p class = "context"><a href="">Хор.-Мневники </a>Круглосуточно</p>
+      <h3>+7 (495) 980-66-22</h3>
+    </div>
+
+  </div>
 </template>
 
 <style scoped lang="scss">
-
+.header{
+  display: flex;
+  justify-content: space-around;
+  .header__locate{
+    p{
+      margin-left: 20px;
+    }
+    img{
+      transform: translateY(17px);
+    }
+  }
+}
 </style>
